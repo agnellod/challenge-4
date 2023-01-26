0a6c39d02a77949bc762de77da24ae2fe22d6ba6
Dylan <agnello.dylan95@gmail.com>
2023-01-25 18:05:47 -0700

    adding java and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # challenge-4
+
+
+## Main
+-first I created a html file
+-then i created a css file to style the page
+-then i added java script to make page functional
+
+- https://github.com/agnellod/challenge-4
+
+![Screenshot_20230125_103754](https://user-images.githubusercontent.com/119445300/214731657-aa0eb21f-5bab-4147-81cf-2b9307d55d57.png)
--- script.js
+++ script.js
@@ -11,54 +11,54 @@
 // THEN I can save my initials and my score
 
 const questions = [
-    { question: 'Arrays in Javascript can be used to store __________.', 
-      answer: 'all of the above', 
-      choices: ['numbers', 'booleans','strings', 'all of the above']
-    },
-    { question: 'Inside which HTML element do we put the javascript?', 
-      answer: '<script>', 
-      choices: ['<h1>','<js>','<script>','<head>']
-    },
-    { question: 'In the code -- setinterval(time(),1000) -- what is time()?', 
-      answer: 'callback function', 
-      choices: ['callback function', 'undefined','variable', 'all of the above']
-    },
-    { question: 'What syntax would call a function?', 
-      answer: 'function()', 
-      choices: ['var function', 'function', 'call function', 'function()']
-    },
-    { question: 'When did javascript first appear?', 
-      answer: '1995', 
-      choices: ['1995','Roaring twenties','2005','2000']
-    },
-    { question: 'What does DOM stand for?', 
-      answer: 'Document Object Model', 
-      choices: ['Do Overnight Modules', 'Document Object Model','Divas Obviously Model','Do Oo Mo']
-    },
-    { question: 'What is getItem commonly used for?', 
-      answer: 'local storage', 
-      choices: ['adding drama','local storage','online shopping','naming a variable']
-    },
-  ]
+  { question: 'Arrays in Javascript can be used to store __________.', 
+    answer: 'all of the above', 
+    choices: ['numbers', 'booleans','strings', 'all of the above']
+  },
+  { question: 'Inside which HTML element do we put the javascript?', 
+    answer: '<script>', 
+    choices: ['<h1>','<js>','<script>','<head>']
+  },
+  { question: 'In the code -- setinterval(time(),1000) -- what is time()?', 
+    answer: 'callback function', 
+    choices: ['callback function', 'undefined','variable', 'all of the above']
+  },
+  { question: 'What syntax would call a function?', 
+    answer: 'function()', 
+    choices: ['var function', 'function', 'call function', 'function()']
+  },
+  { question: 'When did javascript first appear?', 
+    answer: '1995', 
+    choices: ['1995','Roaring twenties','2005','2000']
+  },
+  { question: 'What does DOM stand for?', 
+    answer: 'Document Object Model', 
+    choices: ['Do Overnight Modules', 'Document Object Model','Divas Obviously Model','Do Oo Mo']
+  },
+  { question: 'What is getItem commonly used for?', 
+    answer: 'local storage', 
+    choices: ['adding drama','local storage','online shopping','naming a variable']
+  },
+]
 
-  const startBox = document.querySelector('#start-box');
-  const questionBox = document.querySelector('#question-box');
-  const scoreBox = document.querySelector('#score-box');
-  const leaderboardBox = document.querySelector('#leaderboard-box');
+const startBox = document.querySelector('#start-box');
+const questionBox = document.querySelector('#question-box');
+const scoreBox = document.querySelector('#score-box');
+const leaderboardBox = document.querySelector('#leaderboard-box');
 
 
 function hideBoxes() {
-  startBox.setAttribute("hidden", true);
-  questionBox.setAttribute("hidden", true);
-  scoreBox.setAttribute("hidden", true);
-  leaderboardBox.setAttribute("hidden", true);
+startBox.setAttribute("hidden", true);
+questionBox.setAttribute("hidden", true);
+scoreBox.setAttribute("hidden", true);
+leaderboardBox.setAttribute("hidden", true);
 
 }
 const resultsDiv = document.querySelector('#results-div');
 const resultsOutput = document.querySelector('#results-output');
 
 function hideResultsOutput () {
-  resultsDiv.style.display ="none";
+resultsDiv.style.display ="none";
 }
 
 var countdownID;
@@ -69,95 +69,98 @@ var score = 0;
 document.querySelector("#start-button").addEventListener("click", startQuiz);
 
 function startQuiz() {
-  hideBoxes();
+hideBoxes();
 
-  questionBox.removeAttribute("hidden");
+questionBox.removeAttribute("hidden");
 
-  currentQuestion = 0;
-  
-  displayQuestion();
+currentQuestion = 0;
 
-  time = 60;
+displayQuestion();
 
-  countdownID = setInterval(countdown, 1000);
+time = 60;
 
-  displayTime();
+countdownID = setInterval(countdown, 1000);
+
+displayTime();
 }
 
 
 function countdown(){
-  time--;
-  displayTime();
-  if (time < 1) {
-    endQuiz();
-  }
+time--;
+displayTime();
+if (time < 1) {
+  endQuiz();
+}
+}
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+function displayScore(){
+  scoreDisplay.textContent = score;
 }
 
-const timeDisplay = document.querySelector("#time");
-
-
 function displayTime() {
-  timeDisplay.textContent = time;
+timeDisplay.textContent = time;
 }
 
 function displayQuestion() {
-  let question = questions[currentQuestion];
-  let choices = question.choices;
+let question = questions[currentQuestion];
+let choices = question.choices;
 
-  let h2QuestionElement = document.querySelector("#question-text");
-  h2QuestionElement.textContent = question.question;
+let h2QuestionElement = document.querySelector("#question-text");
+h2QuestionElement.textContent = question.question;
 
-  for (let i = 0; i < choices.length; i++) {
-  let choice = choices[i];
-  let choiceButton = document.querySelector("#choice" + i)
-  choiceButton.textContent = choice;
-    
-  }
+for (let i = 0; i < choices.length; i++) {
+let choice = choices[i];
+let choiceButton = document.querySelector("#choice" + i)
+choiceButton.textContent = choice;
+  
+}
 }
 
 document.querySelector("#quiz-choices").addEventListener("click", checkAnswer);
 
 function choiceIsCorrect(choiceButton) {
-  return choiceButton.textContent === questions[currentQuestion].answer;
+return choiceButton.textContent === questions[currentQuestion].answer;
 }
 
 function checkAnswer(eventObject) {
 
-  let choiceButton = eventObject.target;
+let choiceButton = eventObject.target;
 
-  resultsDiv.style.display = "block";
+resultsDiv.style.display = "block";
 
-  if (choiceIsCorrect(choiceButton)) {
-    resultsOutput.textContent = "Correct!";
-    setTimeout(hideResultsOutput, 1000);
-    score =  score + 5
+if (choiceIsCorrect(choiceButton)) {
+  resultsOutput.textContent = "Correct!";
+  setTimeout(hideResultsOutput, 1000);
+  score =  score + 5
+} 
+
+else {
+  resultsOutput.textContent = "Incorrect!";
+  setTimeout(hideResultsOutput, 1000);
+
+if (time >= 5) {
+    time = time - 5;
+    displayTime();
   } 
-  
-  else {
-    resultsOutput.textContent = "Incorrect!";
-    setTimeout(hideResultsOutput, 1000);
-
-  if (time >= 5) {
-      time = time - 5;
-      displayTime();
-    } 
-  else {
-      time = 0;
-      displayTime();
-      endQuiz();
-    }
-  }
-}
-currentQuestion++;
-  if (currentQuestion < questions.length) {
-    displayQuestion();
-  } else {
+else {
+    time = 0;
+    displayTime();
     endQuiz();
   }
+}
+}
+currentQuestion++;
+if (currentQuestion < questions.length) {
+  displayQuestion();
+} else {
+  endQuiz();
+}
 
 function endQuiz() {
-    clearInterval(countdownID);
-    hideBoxes();
-    scoreBox.removeAttribute("hidden");
-    score.textContent = time;
-  }
+  clearInterval(countdownID);
+  hideBoxes();
+  scoreBox.removeAttribute("hidden");
+  score.textContent = time;
+  displayScore();
+}
